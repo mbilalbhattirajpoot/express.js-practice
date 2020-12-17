af6055d86777808d0d49a2068f914412d65f6648
mbilalbhattirajpoot <m.bilalbhattirajpoot@gmail.com>
2020-12-17 13:12:40 -0800

    #8 data request

--- myapp/datamethods.js
+++ myapp/datamethods.js
@@ -5,16 +5,25 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 app.post('/', function (req, res) {
     res.send('Got a POST request')
-  })
-  app.put('/user', function (req, res) {
+})
+app.put('/user', function (req, res) {
     res.send('Got a PUT request at /user')
-  })
-  app.delete('/user',function(req,res){
-      res.send('got a put request at /user')
-  })
+})
+app.delete('/user', function (req, res) {
+    res.send('got a put request at /user')
+})
+app.use(express.static('public'))
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
