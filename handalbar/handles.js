13250481f8f41fc934d2785907eb2c12b5bfaa09
mbilalbhattirajpoot <m.bilalbhattirajpoot@gmail.com>
2020-12-17 16:11:44 -0800

    #13 handlebars layout

---
+++ handalbar/handles.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+//Loads the handlebars module
+const handlebars = require('express-handlebars');
+//Sets our app to use the handlebars engine
+app.set('view engine', 'handlebars');
+//Sets handlebars configurations (we will go through them later on)
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+}));
+//Serves static files (we need it to import a css file)
+app.use(express.static('public'))
+//Sets a basic route
+app.get('/', (req, res) => res.render('main,{layout:"index'));
+
+//Makes the app listen to port 3000
+app.listen(port, () => console.log(`App listening to port ${port}`));
